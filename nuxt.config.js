92dd7755ee258a2a10f340eb9e2bfa8e0c67dd0e
mikamidies <mikamidies04@gmail.com>
2023-10-16 16:38:19 +0500

    Start

---
+++ nuxt.config.js
@@ -0,0 +1,34 @@
+export default {
+  head: {
+    title: "Mazmoon",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+
+  css: ["@/assets/css/main.css"],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: [],
+
+  modules: ["bootstrap-vue/nuxt", "@nuxtjs/axios", "@nuxtjs/pwa"],
+
+  axios: {
+    baseURL: "/",
+  },
+
+  pwa: {
+    manifest: {
+      lang: "en",
+    },
+  },
+
+  build: {},
+};
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container">
+    <p>Hello World</p>
+  </div>
+</template>
